c052013c889af8fb060866e6938ad3ea40381f66
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-05-05 15:15:51 +0545

    second commit

--- 02-loops/01-only-odds/only-odds.js
+++ 02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+const onlyOdds= (num) => {
+    let sum = 0;
+    
+    for(let i=1; i <= num; ++i){
+
+        if (i % 2 ===1){
+            sum += i;
+        }   
+    }
+    return sum;
+}
+console.log(`the sum of only odds ${onlyOdds(9)}`);
--- 02-loops/02-crazy-caps/crazy-caps.js
+++ 02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+const crazyCaps = (stringValue) => {
+    let result = "";
+
+    for (let i = 0; i < stringValue.length; ++i){
+        if (i % 2 === 1){
+            result += stringValue[i].toUpperCase();
+        }
+        else{
+            result += stringValue[i];
+        }
+    }
+    return result;
+}
+console.log(crazyCaps("this is a javascript programming"));
