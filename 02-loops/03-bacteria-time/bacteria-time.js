a9dac4ef06b6a3a154f1909816889e286bdad527
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-05-06 14:47:05 +0545

    second commit

--- 02-loops/03-bacteria-time/bacteria-time.js
+++ 02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const bacteriaTime = (currentNum, targetNum) =>{
+
+    if ( currentNum > targetNum) return "targetNum must be larger than currentNum";
+
+    let numMinutes = 0;
+
+    while (currentNum < targetNum){
+        numMinutes += 20;
+        currentNum *= 2;
+    }
+    return numMinutes;
+}
+console.log(`the bacteria time is ${bacteriaTime(1,4)}`);
