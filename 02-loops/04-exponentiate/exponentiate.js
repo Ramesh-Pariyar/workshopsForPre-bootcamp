4c29ba76fee234191d000fc3861d59ae6836dac2
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-05-10 15:08:30 +0545

    second comit

--- 02-loops/04-exponentiate/exponentiate.js
+++ 02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+const exponentiate = (base, power) => {
+    let result = 1;
+    if (power >= 0){
+        if (power > 0){
+            result = base ** power;
+
+        }
+    return result
+    }else{
+        return "invalid"
+
+    }
+}
+console.log(exponentiate(2,6));
