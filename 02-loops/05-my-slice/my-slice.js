6aecb3b90fba28bfb56c89422800602de61bd963
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-05-12 14:42:51 +0545

    third commit

--- 02-loops/05-my-slice/my-slice.js
+++ 02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const mySlice = (originalString, startIdx = 0, endIdx = originalString.length) => {
+    let newString = "";
+        for ( let i = startIdx; i < endIdx; i++){
+            const curChar = originalString[i];
+            newString += curChar;
+        }
+        return newString;
+    }
+console.log( mySlice("pokhara is a beautiful place."));
+console.log( mySlice("pokhara is a beautiful place.",5,9));
--- 02-loops/06-my-index-of/my-index-of.js
+++ 02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+const myIndexOf = (source, searchValue, startIdx = 0) => {
+    for (let i = startIdx; i < source.length - searchValue.length; i++){
+        const curTerm = source.slice(i, i + searchValue.length)
+        if ( curTerm === searchValue){
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(myIndexOf("this is pokhara",7));
+console.log(myIndexOf("this is pokhara", "pokh"));
