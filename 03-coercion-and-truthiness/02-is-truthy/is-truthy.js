83d616d95ccd2504dff0356acbe99ccb15de7af8
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-05-16 12:37:32 +0545

    second commit

--- 03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ 03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,23 +1,23 @@
 // YOUR CODE BELOW
 const isTruthy = (value) => {
     
-    if (!!value) return true;
-
+    
     if (value === false){
         return "the boolean value false is falsey";
     }
-
+    
     if (value === null){
         return " the null value is falsey";
     }
-
+    
     if (value === ''){
-        return "the empty string is falsey( the only falsey string";
-    }
-
-    if (value === 0){
-        return "the number 0 is falsey (the only falsey number";
+        return "the empty string is falsey( the only falsey string)";
     }
     
+    if (value === 0){
+        return "the number 0 is falsey (the only falsey number)";
+    }
+    
+    if (!!value) return true;
     return "the undefined is falsey";
 }
--- 03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ 03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+const zeroDarkThirty = (aNumber) =>{
+
+    if (aNumber === 0){
+        return NaN;
+    }
+
+    let originalValue = String(aNumber);
+    let newValue = "";
+    
+    for (let i = 0; i < originalValue.length ; i++){
+        if(originalValue[i] !== "0"){
+        newValue += originalValue[i];
+
+    }
+}
+    return Number(newValue);
+    
+}
+console.log(zeroDarkThirty(502220212022520));
